b9130554b511a1b08dea86c5c9289f3ed602a37e
Logan Gingerich <logangingerich@yahoo.com>
2020-12-11 16:08:09 -0700

    Adding individual user and group views

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/directory/:id', async (req, res) => {
   })
 })
 
-app.get('/directory/:id/groups', async (req, res) => {
+app.get('/directory/:id/usersgroups', async (req, res) => {
   const directories = await workos.directorySync.listDirectories();
   const directory = await directories.data.filter((directory) => {
     return directory.id == req.params.id
@@ -58,6 +58,45 @@ app.get('/directory/:id/groups', async (req, res) => {
     users: users.data,
     title: "Group & Users"
   })
+})
+
+app.get('/directory/:id/group/:groupId', async (req, res) => {
+  const directories = await workos.directorySync.listDirectories();
+  const directory = await directories.data.filter((directory) => {
+    return directory.id == req.params.id
+  })[0]
+  const groups = await workos.directorySync.listGroups({
+    directory: req.params.id,
+  });
+  const group = await groups.data.filter((group) => {
+    return group.id == req.params.groupId
+  })[0]
+  res.render('group', {
+    directory: directory,
+    title: "Directory",
+    group: JSON.stringify(group),
+    rawGroup: group
+  })
+})
+
+app.get('/directory/:id/user/:userId', async (req, res) => {
+  const directories = await workos.directorySync.listDirectories();
+  const directory = await directories.data.filter((directory) => {
+    return directory.id == req.params.id
+  })[0]
+  const users = await workos.directorySync.listUsers({
+    directory: req.params.id,
+  });
+  const user = await users.data.filter((user) => {
+    return user.id == req.params.userId
+  })[0]
+  res.render('user', {
+    directory: directory,
+    title: "Directory",
+    user: JSON.stringify(user),
+    rawUser: user
+  })
+  console.log(user);
 })
 
 /**
